39657f0c6c4822424647977643dd1ba378b76e6c
Robert <demond611@gmail.com>
2017-05-15 07:20:57 -0500

    updated gulpfile.js with postcss-import, started seperation of css files withcss stylling with modules..base folders, updated header and button css files

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvariables = require('postcss-simple-vars'),
-nested = require('postcss-nested');
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 
 
@@ -22,7 +23,7 @@ gulp.task('html', function(){
 gulp.task('styles', function(){
 	
 	return gulp.src('./app/assets/styles/styles.css') // css source
-		.pipe(postcss([nested, cssvariables, autoprefixer])) // css manipuations
+		.pipe(postcss([cssImport, nested, cssvariables, autoprefixer])) // css manipuations
 		.pipe(gulp.dest('./app/temp/styles')); // temp css save location
 
 });
