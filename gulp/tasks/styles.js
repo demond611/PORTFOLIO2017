3eb44789babd1bba5029d5e3aa99559af8c40d6a
Robert <demond611@gmail.com>
2017-05-17 13:32:45 -0500

    updated styles.js with error checking function

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvariables = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
+
+
+gulp.task('styles', function(){
+	// add 'return' to async operations
+	return gulp.src('./app/assets/styles/styles.css') // css source
+		.pipe(postcss([cssImport, nested, cssvariables, autoprefixer])) // css manipuations
+		.on('error', function(msg){ // error checking function on error
+			console.log(msg.toString());
+			this.emit('end');
+		})
+		.pipe(gulp.dest('./app/temp/styles')); // temp css save location
+
+});
