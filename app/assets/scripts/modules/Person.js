1926311e45dfae6a78afcb1edc25f38b7a389de6
Robert <demond611@gmail.com>
2017-05-25 18:41:54 -0500

    updated our features section to display with flexbox

--- app/assets/scripts/modules/Person.js
+++ app/assets/scripts/modules/Person.js
@@ -1,9 +1,15 @@
-function Person(name, favColor){
-	this.name = name;
-	this.favoritecolor = favColor;
-	this.greet = function(){
-		console.log("Hello, my name is "+this.name+" and I love the color "+this.favoritecolor);
+class Person{
+	
+	constructor(name, favColor){
+		this.name = name;
+		this.favoritecolor = favColor;
+	}
+
+	
+	greet(){
+		console.log("Yo yo yo, my name is "+this.name+" and I love the color "+this.favoritecolor);
 	}
 }
 
-module.exports = Person;
+//module.exports = Person;	// NODE WAY TO EXPORT
+export default Person;		// ES6 WAY TO EXPORT
