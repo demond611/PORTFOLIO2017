d2ce21db28409f56e901315a2986972a7598fc93
Robert <demond611@gmail.com>
2017-05-25 19:29:07 -0500

    started javascript/jquery to show/hide menu in mobile view

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,20 +1,3 @@
-var $ = require('jquery'); // ADD JQUERY FUNCTIONALITY
+import MobileMenu from './modules/MobileMenu';
 
-//var Person = require('./modules/Person');	// NODE WAY TO IMPORT
-import Person from './modules/Person'		// ES6 WAY TO IMPORT
-
-class Adult extends Person {
-
-	payTaxes(){
-		console.log(this.name+" owes nothing in taxes.");
-	}
-
-}
-
-
-var robert = new Person('Robert', 'orange');
-robert.greet();
-
-var sha = new Adult('Sha', 'lavender');
-sha.greet();
-sha.payTaxes();
+var mobileMenu = new MobileMenu();
